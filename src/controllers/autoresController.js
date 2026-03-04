e2933dd36942bf1e8a5319ecf45a3c46b1f3c4f5
Douglas Lima <dugbits@gmail.com>
2026-03-04 11:50:40 -0300

    feat: :sparkles: add: feat: Adicionei mais 3 testes no GET de autores/:id/livros

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -72,15 +72,17 @@ class AutoresController {
   static listarLivrosPorAutor = async (req, res) => {
     const { params } = req;
     try {
+      const resultado = await Autor.pegarPeloId(params.id);
+      // adicionando a condição para retornar 404
+      if (!resultado) {
+        return res.status(404).json({ message: `id ${params.id} não encontrado` });
+      }
       const listaLivros = await Autor.pegaLivrosPorAutor(params.id);
-      const autor = await Autor.pegarPeloId(params.id);
-      return res
-        .status(200)
-        .json({ autor, livros: listaLivros });
+      return res.status(200).json({ autor: resultado, livros: listaLivros });
     } catch (err) {
       return res.status(500).json(err.message);
     }
-  }
+  };
 }
 
 export default AutoresController;
